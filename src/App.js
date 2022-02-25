1f2d474fda6b4233f0976fd3ef49cda4020a1c8d
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-02-25 19:40:02 +0000

    adding buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [result, setResult] = useState(0);
+    return (
+        <>
+            <h1>Hola Emilio...!</h1>
+            <form>
+                <input type='text' value={result} />
+            </form>
+            <div className='keypad'>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>0</button>
+                <button>&divide;</button>
+                <button>&ndash;</button>
+                <button>+</button>
+                <button>&times;</button>
+                <button>.</button>
+                <button>=</button>
+                <button>C</button>
+                <button>Clear</button>
+            </div>
+        </>
+    )
 }
 
 export default App;
